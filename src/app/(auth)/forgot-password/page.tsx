7c1cf3f8fcf7dd181651bba483074ac08c527457
Zhanna <kezhanna.03@mail.ru>
2025-06-25 16:39:39 +0500

    route groups

---
+++ src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,10 @@
+import '../../globals.css'
+
+export default function ForgotPassword(){
+    return (
+        <label htmlFor="" className="">
+            <input type="text" className="w- p-[6px] px-[30px] border-[red] border-[1px] rounded-[12px] focus:outline-none focus:ring-2 focus:ring-[pink] text-[grey]" placeholder="Write your name" />
+        </label>
+        
+    )
+}

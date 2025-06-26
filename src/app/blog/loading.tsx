6318b52acce115aefe208889638a18d5c222cf26
Zhanna <kezhanna.03@mail.ru>
2025-06-26 12:49:55 +0500

    Loading UI, loading.tsx

---
+++ src/app/blog/loading.tsx
@@ -0,0 +1,8 @@
+import '../globals.css'
+
+export default function Loading(){
+    return <div className="relative">
+        <img src="https://framerusercontent.com/images/Bs9orMbLOtB3NefDwB6zUODE.jpg" alt="" />
+        <p className="absolute top-[0] left-[500px] text-[80px] text-[white] font-[bold]">Loading</p>
+    </div>
+}
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,6 +4,11 @@ export const metadata:Metadata = {
     title: 'Blog',
 }
 
-export default function Blog(){
+export default async function Blog(){
+    await new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve('international delay');
+        }, 2000);
+    })
     return "Blog";
 }

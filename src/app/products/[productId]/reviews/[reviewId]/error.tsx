b15d9e0037984ffc64670ceaddbc0fbd56cda61d
Zhanna <kezhanna.03@mail.ru>
2025-06-26 14:33:12 +0500

    Recovering from errors, reload(), useRouter(), startTransition()

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,7 +1,26 @@
-'use client'
+"use client";
+import "../../../../globals.css";
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
 
-export default function ErrorBoundary({error}:{
-    error: Error
-}){
-    return <div>{error.message}</div>;
+export default function ErrorBoundary({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+    const router =useRouter();
+    const reload = ()=> {startTransition(()=>{
+        router.refresh();
+        reset();
+    })}
+  return (
+    <div>
+      {error.message} <br />
+      <button onClick={() => reload()} className="bg-[orange] text-[white] rounded-[8px] px-[20px] py-[5px] my-[10px]">
+        Try again
+      </button>
+    </div>
+  );
 }

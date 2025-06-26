927e962e41be9c911cda6591ec89c0ef4f3358ba
Zhanna <kezhanna.03@mail.ru>
2025-06-26 09:33:01 +0500

    Routing Metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,5 +1,44 @@
-export default async function ProductDetails({params,}: {params: Promise<{productId: string}>;
-}){
-    const productId= (await params).productId;
-    return <h1>Details about {productId}</h1>;
+// import { Metadata } from "next";
+
+// type Props = {
+//     params: Promise <{productId: string}>;
+// };
+
+// export const generateMetaData = async ({params}: Props): Promise<Metadata> => {
+//     const id = (await params).productId;
+//     return{
+//         title: `Product ${id}`,
+//     };
+// };
+
+// export default async function ProductDetails({params}: Props ){
+//     const productId= (await params).productId;
+//     return <h1>Details about {productId}</h1>;
+// }
+
+import { Metadata } from "next";
+
+type Props = {
+  params: { productId: string };
+};
+
+// ✅ Dynamic page title using `generateMetadata`
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const id = params.productId;
+  const title = await new Promise((resolve)=>{
+    setTimeout(()=>{
+        resolve(`Iphone ${id}`)
+    }, 100)
+  })
+  return {
+    title: `Product ${id}`,
+  };
+};
+
+// ✅ Page Component
+export default async function ProductDetails({ params }: Props) {
+  const productId = params.productId;
+  return <h1>Details about {productId}</h1>;
 }
